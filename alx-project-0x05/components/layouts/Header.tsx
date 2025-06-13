c64f84cc76df6c775f7a7df69c83c7a5aef170b6
trizahc <trizahchepkirui07@gmail.comm>
2025-06-13 08:10:18 -0500

    made new changes

--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-  const pathname = usePathname();
-  const { count } = useCount();
+
+  const pathname = usePathname()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
     <header className="fixed w-full bg-white shadow-md">
@@ -14,15 +17,24 @@ const Header: React.FC = () => {
           Splash App
         </Link>
 
+        {/* Button Group */}
         <div className="flex gap-4">
-          {!["/counter-app"].includes(pathname) ? (
-            <>
-              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
-            </>
-          ) : (
-            <p className="font-semibold text-lg">Current count : {count}</p>
-          )}
+          {
+            !["/counter-app"].includes(pathname) ? (
+              <>
+              <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+          />
+          <Button
+            buttonLabel="Sign Up"
+            buttonBackgroundColor="blue"
+          /></>
+            ) : (
+              <p className=" font-semibold text-lg">Current count : {count}</p>
+            )
+          }
+
         </div>
       </div>
     </header>
--- store/store.ts
+++ store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice  } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 
 const counterSlice = createSlice({
@@ -7,11 +7,11 @@ const counterSlice = createSlice({
     value: 0
   },
   reducers: {
-    increment: (state) => {
-      state.value += 1;
+    increment: (state) =>{
+      state.value += 1
     },
     decrement: (state) => {
-      state.value > 0 ? (state.value -= 1) : 0;
+      state.value > 0 ? state.value -= 1 : 0
     }
   }
 });
@@ -20,13 +20,13 @@ const store = configureStore({
   reducer: {
     counter: counterSlice.reducer
   }
-});
+})
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>()
 
-export default store;
+export default store
